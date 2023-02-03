48607d9e946fe10608c533270ee4e180f8d6cb68
Farid-Abishov <abisovfarid14@gmail.com>
2023-02-03 13:20:34 +0400

    for

--- js-foundation/src/js/app.js
+++ js-foundation/src/js/app.js
@@ -5,7 +5,16 @@
 // }
 // console.log(Student);
 
-function slm(){
-    console.log("hello world");
+// function slm(){
+//     console.log("hello world");
+// }
+// slm()
+
+
+var Person=["Ferid","Abishov",20,null,undefined]
+console.log(Person);
+
+cem=0;
+for( let i=0;i<10;i++){
+    console.log(cem=cem+i);
 }
-slm()
