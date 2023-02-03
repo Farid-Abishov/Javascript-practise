5e43012b698e87746d376f756b82eea0023f721c
Farid-Abishov <abisovfarid14@gmail.com>
2023-02-03 13:07:04 +0400

    function

--- js-foundation/src/js/app.js
+++ js-foundation/src/js/app.js
@@ -1,6 +1,11 @@
-var Student ={
-    name:"Farid",
-    surname:"Abishov",
-    age:20,
+// var Student ={
+//     name:"Farid",
+//     surname:"Abishov",
+//     age:20,
+// }
+// console.log(Student);
+
+function slm(){
+    console.log("hello world");
 }
-console.log(Student);
+slm()
