215d3da2eaf4039e03d503f511d31079839ef575
Farid-Abishov <abisovfarid14@gmail.com>
2023-02-09 00:09:16 +0400

    function greeting

--- js-foundation/src/js/app.js
+++ js-foundation/src/js/app.js
@@ -1,22 +1,8 @@
-function greating(msg){
-    console.log(msg);
-}
-greating("Hi")
-greating("how are u")
-greating("what is ur name")
-
-function yashesablama(dogumili){
-    console.log(new Date().getFullYear()-dogumili)
+function greet(name) {
+    console.log("hello"+" "+name);
 }
 
-yashesablama(2002);
-yashesablama(2003);
-yashesablama(2000);
-yashesablama(1980);
-yashesablama(2010);
-
-let a,b;
-function sahehesablama(a,b){
-    console.log(a*b)
-}
-sahehesablama(4,5)
+greet("Farid");
+greet("ALi");
+greet("Murad");
+greet("Joe");
